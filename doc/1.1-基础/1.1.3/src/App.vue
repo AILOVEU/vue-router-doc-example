9ef3a07368b254f2aafde35f6366b0627129a52c
AILOVEU <1032290763@qq.com>
2020-06-15 22:53:57 +0800

    编程式导航1

--- doc/1.1-基础/1.1.3/src/App.vue
+++ doc/1.1-基础/1.1.3/src/App.vue
@@ -1,32 +1,34 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <h1>首页</h1>
+      <button @click="goHome">跳转home</button>
+      <button @click="goAbout">跳转about</button>
+      <h3>{{$route}}</h3>
+      <router-link to="/home">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
   </div>
 </template>
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+    }
+  },
+  methods: {
+    //和a标签不同的是，连续点击两次，会报错
+    //Error: Avoided redundant navigation to current location: "/home".
+    goHome(){
+      this.$router.push('home');
+    },
+    goAbout(){
+      this.$router.push('about');
+    }
+  }
 
-#nav {
-  padding: 30px;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
